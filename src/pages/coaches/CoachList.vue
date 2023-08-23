d1dc5f6fe31504508fff1beaa881c4e467240ec7
Mike <mikhail.yakhyayeu@gmail.com>
2023-08-23 22:17:14 +0300

    coach list. requests sent and displayed

--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -1,12 +1,12 @@
 <template>
-    <sector>
-        FILTER
-    </sector>
-    <sector>
+    <section>
+        <coach-filter @change-filter="setFilters"></coach-filter>
+    </section>
+    <section>
         <base-card>
         <div class="controls">
             <base-button mode="outline">Refresh</base-button>
-            <base-button link to="/register">Register as Coach</base-button>
+            <base-button v-if="!isCoach" link to="/register">Register as Coach</base-button>
         </div>
         <ul v-if="hasCoaches">
             <coach-item
@@ -21,20 +21,55 @@
         </ul>
         <h3 v-else>No coaches found.</h3>
         </base-card>
-    </sector>
+    </section>
 </template>
 
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue';
+import CoachFilter from '../../components/coaches/CoachFilter.vue';
 
 export default ({
-    components: { CoachItem },
+    components: { 
+        CoachItem, 
+        CoachFilter 
+    },
+    data() {
+        return {
+            activeFilters: {
+                frontend: true,
+                backend: true,
+                career: true,
+            },
+        };
+    },
     computed: {
+        isCoach() {
+            return this.$store.getters['coaches/isCoach'];
+        },
         filteredCoaches() {
-            return this.$store.getters['coaches/coaches'];
+            const coaches =  this.$store.getters['coaches/coaches'];
+
+            return coaches.filter(coach => {
+                if(this.activeFilters.frontend && coach.areas.includes('frontend')) {
+                    return true;
+                }
+                if(this.activeFilters.backend && coach.areas.includes('backend')) {
+                    return true;
+                }
+                if(this.activeFilters.career && coach.areas.includes('career')) {
+                    return true;
+                }
+
+                return false;
+            });
         },
         hasCoaches() {
             return this.$store.getters['coaches/hasCoaches'];
+        }
+    },
+    methods: {
+        setFilters(updatedFilters) {
+            this.activeFilters = updatedFilters;
         }
     }
 })
